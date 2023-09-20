de7746ec78e2b322b3a487c795eba67a1770001f
shehan2000 <maliths.20@cse.mrt.lk>
2023-09-20 12:07:12 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {PORT} from './config.js';
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+
+});
